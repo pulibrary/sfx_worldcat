a0fbac985a93c4403f0b6043104b76537fbff8f5
pmgreen <pmgreen@princeton.edu>
2019-10-24 12:17:13 -0400

    Cleaned up first two tests, added helper method, made them pass

--- spec/unit/pinyin_spec.rb
+++ spec/unit/pinyin_spec.rb
@@ -4,20 +4,21 @@ require 'byebug'
 
 describe 'SFXWorldcat::string_to_pinyin' do
   let(:dict) { SFXWorldcat::chi_dict }
-
+  let(:chinese_tag) { '880' }
+  let(:linking_subf) { '6' }
   
   context "first pinyin_sample" do
     let(:marc_record) do
-      reader = MARC::Reader.new('spec/fixtures/pinyin_sample.mrc')
+      reader = MARC::Reader.new('spec/fixtures/pinyin_number_sample.mrc')
       reader.first
     end
 
-    it "converts pinyin" do
-      tag = '245'
-      subfield_code = 'p'
+    it 'converts pinyin: changing Chinese numbers into Arabic numerals' do
+      tag = '264'
+      subfield_code = 'b'
 
       # save off chinese
-      chinese_str = marc_record['880'][subfield_code]
+      chinese_str = extract_chinese_from_field(tag, subfield_code) 
       
       # save off the pinyin
       pinyin_str = marc_record[tag][subfield_code]
@@ -30,12 +31,12 @@ describe 'SFXWorldcat::string_to_pinyin' do
       expect(output).to eq(pinyin_str)
     end
 
-    it "converts pinyin" do
+    it 'converts pinyin without numbers in record' do
       tag = '245'
       subfield_code = 'a'
 
       # save off chinese
-      chinese_str = marc_record['880'][subfield_code]
+      chinese_str = extract_chinese_from_field(tag, subfield_code)
       
       # save off the pinyin
       pinyin_str = marc_record[tag][subfield_code]
@@ -47,5 +48,9 @@ describe 'SFXWorldcat::string_to_pinyin' do
 
       expect(output).to eq(pinyin_str)
     end
+  end
+  def extract_chinese_from_field(tag, subfield_code)
+    chinese_field = marc_record.fields.select { |field| field.tag == chinese_tag && field[linking_subf] =~ /^#{tag}/ }.first
+    chinese_field[subfield_code]
   end
 end

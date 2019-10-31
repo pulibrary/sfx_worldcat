bd2ca2bee2ddd746faa80c24783097d71345ce5d
Carolyn Cole <cac9@princeton.edu>
2019-10-31 15:02:57 -0400

    Started refactoring

--- spec/unit/pinyin_spec.rb
+++ spec/unit/pinyin_spec.rb
@@ -25,7 +25,7 @@ describe 'SFXWorldcat::string_to_pinyin' do
 
       # run the chinese through string_to_pinyin
       output = SFXWorldcat.string_to_pinyin(chinese_str,
-                                            tag, { ind1: '0', ind2: '0' },
+                                            tag, indicator_hash(marc_record: marc_record, tag: tag),
                                             subfield_code, dict)
 
       expect(output).to eq(pinyin_str)
@@ -43,12 +43,46 @@ describe 'SFXWorldcat::string_to_pinyin' do
 
       # run the chinese through string_to_pinyin
       output = SFXWorldcat.string_to_pinyin(chinese_str,
-                                            tag, { ind1: '0', ind2: '0' },
+                                            tag, indicator_hash(marc_record: marc_record, tag: tag),
                                             subfield_code, dict)
 
       expect(output).to eq(pinyin_str)
     end
   end
+
+  context 'first pinyin_sample' do
+    let(:marc_record) do
+      reader = MARC::Reader.new('spec/fixtures/3163341.mrc')
+      reader.first
+    end
+
+    it 'converts pinyin: changing Chinese numbers into Arabic numerals' do
+      tag = '245'
+      subfield_code = 'p'
+
+      # save off chinese
+      chinese_str = extract_chinese_from_field(tag, subfield_code)
+
+      # save off the pinyin
+      pinyin_str = marc_record[tag][subfield_code]
+
+      # run the chinese through string_to_pinyin
+      output = SFXWorldcat.string_to_pinyin(chinese_str,
+                                            tag, indicator_hash(marc_record: marc_record, tag: tag),
+                                            subfield_code, dict)
+
+      expect(output).to eq(pinyin_str)
+    end
+  end
+
+  def indicator_hash(marc_record:, tag:)
+    ind1 =  marc_record[tag].indicator1
+    ind2 =  marc_record[tag].indicator2
+
+    { ind1: ind1, ind2: ind2 }
+  end
+
+
   def extract_chinese_from_field(tag, subfield_code)
     chinese_field = marc_record.fields.select { |field| field.tag == chinese_tag && field[linking_subf] =~ /^#{tag}/ }.first
     chinese_field[subfield_code]
